1236240f3821b0353ab91cc1a725266941b85bf0
Jessica Kroner <106697199+Kronerwebentwicklung@users.noreply.github.com>
2022-06-20 14:36:40 +0200

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,40 @@
+function myFunction() {
+  document.getElementById("text").innerHTML = "1";
+}
+
+function myFunction() {
+  document.getElementById("text").innerHTML = "2";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "3";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "4";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "5";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "6";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "7";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "8";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "9";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "+";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "-";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = "=";
+}
+function myFunction() {
+  document.getElementById("text").innerHTML = ",";
+}
